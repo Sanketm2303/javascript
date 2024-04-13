6eee1d647f7ceb161c36acdc66092a1b4aa30dbe
Sanketm2303 <138425949+Sanketm2303@users.noreply.github.com>
2024-04-13 10:02:32 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code in newer version 
+
+// alert (3+3) we are using node js not browser
+
+let name = "saan"
+let age  = 22
+let isLoggedIn = false
+
+// number =>  2 power of 53
+// bigint 
+// string => ""
+// isBoolen => true/false
+// null = standlone value
+// undefined => 
+// symbol => uniqueness 
+
+// object 
+
+console.log(typeof age);
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
+
+ 
