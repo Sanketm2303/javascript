cc91acdf0e1c53d470c54334c0649cf3b65ee219
Sanketm2303 <138425949+Sanketm2303@users.noreply.github.com>
2024-04-14 08:50:52 +0000

    dataTypeSummary.js

--- 01_basics/dataTypeSummary.js
+++ 01_basics/dataTypeSummary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+/************************************ Memory : Stack & heap *****************************************/
+// premitive datatypes : Stored in stack, 
+// Non-premitive datatypes : Stores in heap.
+
+let myUsername = "saanm2303"
+
+let newUserName = myUsername
+newUserName = "coolsaan"
+
+console.log(myUsername);
+console.log(newUserName);   // example of stack
+
+
+let userOne = {
+    email : "sanm2303.10@gmail.com",
+    upi : "san@ybl"
+}
+
+let userTwo = userOne 
+userTwo.email = "coolsan@gmail.com"
+
+console.log(userOne.email);
+console.log(userOne.email);
+
