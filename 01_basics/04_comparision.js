79490ce4e40d1f113ee062ef241028f4399323f7
Sanketm2303 <138425949+Sanketm2303@users.noreply.github.com>
2024-04-14 08:24:31 +0000

    04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);   // it is true because comparision operator >=,<= converts null into number,treating it as a number
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
